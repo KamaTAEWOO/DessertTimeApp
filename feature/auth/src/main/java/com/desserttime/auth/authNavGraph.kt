45ceb23fdd2520a6fb6c24d3771c514dea6d30b6
kamamerona <zszs2020@naver.com>
2024-07-06 23:10:37 +0900

    [feat] #8 auth로 병합 완료, 앱꺼지는 현상 완료

--- feature/auth/src/main/java/com/desserttime/auth/authNavGraph.kt
+++ feature/auth/src/main/java/com/desserttime/auth/authNavGraph.kt
@@ -4,6 +4,8 @@ import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.composable
 import androidx.navigation.navigation
+import com.desserttime.auth.inquiry.InquiryCompleteScreen
+import com.desserttime.auth.inquiry.InquiryInputScreen
 import com.desserttime.auth.login.LoginScreen
 import com.desserttime.auth.signup.SignUpAgreeScreen
 import com.desserttime.auth.signup.SignUpChooseScreen
@@ -65,6 +67,29 @@ fun NavGraphBuilder.authNavGraph(
 ////                    navHostController.popBackStack()
 ////                }
 //            )
+        }
+
+        composable(route = AuthDestination.InquiryInput.route) {
+            InquiryInputScreen(
+                onNavigateToInquiryInput= {
+                    navHostController.navigate(AuthDestination.InquiryComplete.route)
+                },
+                onBack = {
+                    navHostController.popBackStack()
+                }
+            )
+        }
+
+        composable(route = AuthDestination.InquiryComplete.route) {
+            InquiryCompleteScreen(
+                onNavigateToLogin = {
+                    navHostController.navigate(AuthDestination.Login.route) {
+                        popUpTo(AuthDestination.InquiryComplete.route) {
+                            inclusive = true
+                        }
+                    }
+                },
+            )
         }
     }
 }
