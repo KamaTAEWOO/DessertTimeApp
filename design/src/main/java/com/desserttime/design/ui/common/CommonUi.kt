33520c75588a1b5cded912f12234507c7aab5c8f
kamamerona <zszs2020@naver.com>
2024-07-21 14:08:45 +0900

    [feat] #12 GrayLine 공통 함수로 변경

---
+++ design/src/main/java/com/desserttime/design/ui/common/CommonUi.kt
@@ -0,0 +1,21 @@
+package com.desserttime.design.ui.common
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+object CommonUi {
+
+    // *********************************************************************************************
+    // *************************************Public Method*******************************************
+    // *********************************************************************************************
+
+    // 회색 라인
+    @Composable
+    fun GrayLine(modifier: Modifier) {
+        Box(
+            modifier = modifier
+        )
+    }
+}
