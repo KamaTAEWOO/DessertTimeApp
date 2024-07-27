3473107e2ec5ed97d318a46257e2da60342a8f31
kamamerona <zszs2020@naver.com>
2024-07-27 20:34:30 +0900

    [feat] #19 각 페이지 개발 완료

---
+++ feature/auth/src/main/java/com/desserttime/main/MainControlScreen.kt
@@ -0,0 +1,18 @@
+package com.desserttime.main
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun MainControlScreen(
+
+) {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MainControlScreenPreview() {
+    MainControlScreen(
+    )
+}
