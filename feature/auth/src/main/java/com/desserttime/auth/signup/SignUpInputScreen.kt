3767047699d0499662349d0207ce7cce8c0063b8
kamamerona <zszs2020@naver.com>
2024-07-28 13:23:09 +0900

    [feat] #21 import 정리

--- feature/auth/src/main/java/com/desserttime/auth/signup/SignUpInputScreen.kt
+++ feature/auth/src/main/java/com/desserttime/auth/signup/SignUpInputScreen.kt
@@ -28,7 +28,6 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.desserttime.design.R
-import com.desserttime.design.theme.AltoAgree
 import com.desserttime.design.theme.AthensGray
 import com.desserttime.design.theme.Black30
 import com.desserttime.design.theme.Black60
