42b79072bd189c820c4c0eeba370bebc15a69ada
kamamerona <zszs2020@naver.com>
2024-07-31 21:55:48 +0900

    [feat] #24 ktlint 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,21 @@ plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
     alias(libs.plugins.androidLibrary) apply false
+    alias(libs.plugins.ktlint) apply false
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.kapt) apply false
+}
+
+allprojects {
+    apply {
+        plugin("org.jlleitschuh.gradle.ktlint")
+//        plugin("io.gitlab.arturbosch.detekt")
+    }
+
+    afterEvaluate {
+//        detekt {
+//            buildUponDefaultConfig = true
+//            config.setFrom(files("$rootDir/detekt-config.yml"))
+//        }
+    }
 }
