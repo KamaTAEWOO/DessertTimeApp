42c8c60765c031e8d96fc1cf22fb2d784617ff7b
kamamerona <zszs2020@naver.com>
2024-08-04 00:34:10 +0900

    [feat] #23 import 정리

--- feature/mypage/src/main/java/com/desserttime/mypage/MyPageScreen.kt
+++ feature/mypage/src/main/java/com/desserttime/mypage/MyPageScreen.kt
@@ -1,12 +1,9 @@
 package com.desserttime.mypage
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
