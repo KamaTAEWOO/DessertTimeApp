128e382db2f2e0c8317d1ea762b7bfc6147d294c
kamamerona <zszs2020@naver.com>
2024-08-04 13:53:45 +0900

    [feat] #27 content 추가

--- feature/category/src/main/java/com/desserttime/category/CategoryScreen.kt
+++ feature/category/src/main/java/com/desserttime/category/CategoryScreen.kt
@@ -2,7 +2,9 @@ package com.desserttime.category
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -10,15 +12,39 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.desserttime.design.R
 import com.desserttime.design.ui.common.AppBarUi
 
 @Composable
-fun CategoryScreen() {
-    Box(modifier = Modifier.fillMaxSize().background(Color.White)) {
+fun CategoryScreen(
+    categoryViewModel: CategoryViewModel = hiltViewModel()
+) {
+    Column {
+        AppBarUi.AppBar(stringResource(id = R.string.txt_bottom_category))
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.White)
+        ) {
+            item {
+                CategoryContent(categoryViewModel)
+            }
+        }
+    }
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .background(Color.White)) {
         AppBarUi.AppBar(stringResource(id = R.string.txt_bottom_category))
         Text("Category Screen", modifier = Modifier.align(Alignment.Center))
     }
+}
+
+@Composable
+fun CategoryContent(
+    categoryViewModel: CategoryViewModel = hiltViewModel()
+) {
+    Text("Category Content")
 }
 
 @Preview(showBackground = true)
