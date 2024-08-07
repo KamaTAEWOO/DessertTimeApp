de7b6ba70bbb6dc00fca148a8e7a01dce5d953d6
KamaTAEWOO <zszs2020@naver.com>
2024-08-07 13:56:57 +0900

    [feat] #33 점수, 후기작성, 메뉴사진, 작성완료 버튼 UI

--- feature/review/src/main/java/com/desserttime/review/ReviewWriteScreen.kt
+++ feature/review/src/main/java/com/desserttime/review/ReviewWriteScreen.kt
@@ -1,19 +1,29 @@
 package com.desserttime.review
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
@@ -26,26 +36,34 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.desserttime.design.R
 import com.desserttime.design.theme.Alto
+import com.desserttime.design.theme.AltoAgree
 import com.desserttime.design.theme.AzureRadiance
 import com.desserttime.design.theme.Black30
 import com.desserttime.design.theme.DessertTimeTheme
 import com.desserttime.design.theme.DoveGray
+import com.desserttime.design.theme.DustyGray
+import com.desserttime.design.theme.TundoraCategory
 import com.desserttime.design.theme.WildSand
 import com.desserttime.design.ui.common.AppBarUi
+import com.desserttime.design.ui.common.CommonUi
 
-@OptIn(ExperimentalLayoutApi::class)
+@OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("UnusedBoxWithConstraintsScope")
 @Composable
 fun ReviewWriteScreen() {
+    val scrollState = rememberScrollState()
+
     Column(
         modifier = Modifier
             .fillMaxSize()
             .background(Color.White)
+            .verticalScroll(scrollState) // Add vertical scrolling
     ) {
         var inputStoreName by remember { mutableStateOf("") }
         var inputMenuName by remember { mutableStateOf("") }
@@ -109,9 +127,93 @@ fun ReviewWriteScreen() {
         Box(modifier = Modifier.padding(top = 8.dp))
         MaterialItemList(materialArr)
         // 점수
+        Spacer(modifier = Modifier.padding(top = 12.dp))
+        Text(
+            text = stringResource(id = R.string.txt_review_write_score),
+            style = DessertTimeTheme.typography.textStyleBold14,
+            color = DoveGray,
+            modifier = Modifier
+                .wrapContentSize()
+                .padding(start = 16.dp)
+        )
+        Box(modifier = Modifier.padding(top = 8.dp))
+        ScoreCheck()
         // 후기 작성
+        Spacer(modifier = Modifier.padding(top = 20.dp))
+        Text(
+            text = stringResource(id = R.string.txt_review_write_behind),
+            style = DessertTimeTheme.typography.textStyleBold14,
+            color = DoveGray,
+            modifier = Modifier
+                .wrapContentSize()
+                .padding(start = 16.dp)
+        )
+        Spacer(modifier = Modifier.padding(top = 8.dp))
+        OutlinedTextField(
+            value = inputReviewBehind,
+            onValueChange = { newText -> inputReviewBehind = newText },
+            textStyle = DessertTimeTheme.typography.textStyleRegular16,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(108.dp)
+                .padding(horizontal = 16.dp),
+            placeholder = {
+                Text(
+                    text = stringResource(id = R.string.txt_inquiry_content_hint),
+                    style = DessertTimeTheme.typography.textStyleRegular12,
+                    color = Black30
+                )
+            },
+            colors = TextFieldDefaults.textFieldColors(
+                containerColor = WildSand,
+                focusedIndicatorColor = AzureRadiance,
+                unfocusedIndicatorColor = Black30
+            )
+        )
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 4.dp) // Column 전체에 위쪽 패딩 추가
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.End // 오른쪽 정렬
+            ) {
+                Text(
+                    text = stringResource(id = R.string.txt_review_write_text_count),
+                    style = DessertTimeTheme.typography.textStyleRegular10,
+                    color = TundoraCategory,
+                    modifier = Modifier.padding(end = 20.dp) // 오른쪽 패딩 추가
+                )
+            }
+        }
         // 메뉴 사진
+        Spacer(modifier = Modifier.padding(top = 5.dp))
+        Text(
+            text = stringResource(id = R.string.txt_review_write_menu_image),
+            style = DessertTimeTheme.typography.textStyleBold14,
+            color = DoveGray,
+            modifier = Modifier
+                .wrapContentSize()
+                .padding(start = 16.dp)
+        )
+        Spacer(modifier = Modifier.padding(top = 8.dp))
+        MenuPicture()
         // 작성완료
+        Spacer(modifier = Modifier.padding(top = 20.dp))
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(start = 16.dp, end = 16.dp, bottom = 20.dp),
+            verticalArrangement = Arrangement.Bottom
+        ) {
+            CommonUi.NextButton(
+                text = stringResource(R.string.txt_review_write_complete),
+                onClick = {},
+                background = AltoAgree,
+                textColor = DustyGray
+            )
+        }
     }
 }
 
@@ -200,6 +302,58 @@ fun MaterialItemRound(
                 .wrapContentWidth()
                 .padding(horizontal = 10.dp, vertical = 5.dp)
         )
+    }
+}
+
+@Composable
+fun ScoreCheck() {
+    val imageResource = painterResource(id = R.drawable.ic_star_off)
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp)
+    ) {
+        repeat(4) {
+            Image(
+                painter = imageResource,
+                contentDescription = stringResource(id = R.string.img_review_write_score_description)
+            )
+        }
+    }
+}
+
+@Composable
+fun MenuPicture() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .size(76.dp)
+                .background(WildSand)
+                .border(1.dp, Alto, RoundedCornerShape(9.dp))
+                .clip(RoundedCornerShape(9.dp))
+                .padding(top = 15.dp, start = 26.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_camera),
+                contentDescription = stringResource(id = R.string.img_review_write_menu_image_description),
+                modifier = Modifier
+                    .size(24.dp)
+            )
+            Text(
+                text = stringResource(id = R.string.txt_review_write_menu_image_count),
+                style = DessertTimeTheme.typography.textStyleMedium12,
+                color = Color.Gray,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .align(Alignment.Center)
+                    .padding(top = 4.dp)
+            )
+        }
     }
 }
 
