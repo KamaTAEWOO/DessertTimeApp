da6467ae1782b80e75fadacba090b8d6894d799c
kamamerona <zszs2020@naver.com>
2024-08-24 15:17:53 +0900

    [build] #48 디자인 모듈에서 timber 라이브러리 사용

--- design/build.gradle.kts
+++ design/build.gradle.kts
@@ -37,6 +37,8 @@ android {
 
 dependencies {
 
+    implementation(libs.timber)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
