3b6689dd80c2058b35cf8eb45fc1c51dd59db212
KamaTAEWOO <zszs2020@naver.com>
2024-08-29 17:03:41 +0900

    [feat] #58 ReviewScreen 화면에서 삭제하기 버튼 기능 구현 중

--- feature/review/src/main/java/com/desserttime/review/ReviewScreen.kt
+++ feature/review/src/main/java/com/desserttime/review/ReviewScreen.kt
@@ -1,23 +1,35 @@
 package com.desserttime.review
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -27,6 +39,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.zIndex
 import com.desserttime.design.R
 import com.desserttime.design.theme.Black60
@@ -180,6 +193,7 @@ fun ReviewItemView(
 fun ReviewItem(
     onNavigateToReviewWrite: () -> Unit
 ) {
+    var showDialog by remember { mutableStateOf(false) }
     Row(
         modifier = Modifier
             .fillMaxWidth()
@@ -225,12 +239,67 @@ fun ReviewItem(
                 }
                 // Image -> 클릭 시 삭제하기 팝업창 Image 위에나 아래에 떠야함.
                 IconButton(
-                    onClick = {}
+                    onClick = { showDialog = true }
                 ) {
                     Icon(
                         painter = painterResource(id = R.drawable.ic_review_delete),
                         contentDescription = stringResource(id = R.string.txt_review_delete)
                     )
+                }
+
+                // 팝업이 표시될 때
+                if (showDialog) {
+                    ReviewItemDeletePopup(
+                        onDeleteConfirm = {
+                            // 삭제 확인 로직을 여기에 추가
+                            showDialog = false // 팝업 닫기
+                        },
+                        onDismiss = {
+                            showDialog = false // 팝업 닫기
+                        }
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ReviewItemDeletePopup(
+    onDeleteConfirm: () -> Unit, // 삭제 확인 시 호출되는 콜백 함수
+    onDismiss: () -> Unit // 팝업 닫기 시 호출되는 콜백 함수
+) {
+    Dialog(onDismissRequest = { onDismiss() }) {
+        Surface(
+            shape = RoundedCornerShape(8.dp),
+            color = Color.White,
+            modifier = Modifier
+                .width(93.dp) // 팝업 너비 설정
+                .height(34.dp) // 팝업 높이 설정
+        ) {
+            Column(
+                modifier = Modifier.padding(8.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                TextButton(onClick = { onDeleteConfirm() }) {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.Center
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Delete, // 기본 제공 삭제 아이콘
+                            contentDescription = stringResource(id = R.string.txt_review_delete),
+                            tint = MainColor,
+                            modifier = Modifier.size(16.dp)
+                        )
+                        Spacer(modifier = Modifier.width(4.dp))
+                        Text(
+                            text = stringResource(id = R.string.txt_review_delete), // "삭제하기"
+                            color = MainColor,
+                            style = DessertTimeTheme.typography.textStyleRegular20
+                        )
+                    }
                 }
             }
         }
