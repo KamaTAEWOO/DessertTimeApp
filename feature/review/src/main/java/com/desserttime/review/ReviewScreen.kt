dcb5e7442631a3bd4178d2f59df93c467682ce89
kamamerona <zszs2020@naver.com>
2024-08-29 22:37:01 +0900

    [feat] #58 review item delete 완료

--- feature/review/src/main/java/com/desserttime/review/ReviewScreen.kt
+++ feature/review/src/main/java/com/desserttime/review/ReviewScreen.kt
@@ -1,6 +1,7 @@
 package com.desserttime.review
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -33,21 +34,28 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.layout.positionInWindow
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.Popup
 import androidx.compose.ui.zIndex
 import com.desserttime.design.R
 import com.desserttime.design.theme.Black60
 import com.desserttime.design.theme.DessertTimeTheme
+import com.desserttime.design.theme.Gallery
 import com.desserttime.design.theme.MainColor
 import com.desserttime.design.theme.TundoraCategory
 import com.desserttime.design.theme.WildSand
 import com.desserttime.design.ui.common.AppBarUi
+import timber.log.Timber
 
 @Composable
 fun ReviewScreen(
@@ -194,6 +202,8 @@ fun ReviewItem(
     onNavigateToReviewWrite: () -> Unit
 ) {
     var showDialog by remember { mutableStateOf(false) }
+    var buttonOffset by remember { mutableStateOf(Offset.Zero) }
+
     Row(
         modifier = Modifier
             .fillMaxWidth()
@@ -225,18 +235,18 @@ fun ReviewItem(
         ) {
             Row(
                 verticalAlignment = Alignment.CenterVertically,
-                modifier = Modifier.align(Alignment.CenterEnd)
+                modifier = Modifier
+                    .align(Alignment.CenterEnd)
+                    .onGloballyPositioned { coordinates ->
+                        buttonOffset = coordinates.positionInWindow()
+                    }
             ) {
-                // Image -> 클릭 시 다음 화면 전환 / item의 id를 넘겨줘야함.
-                IconButton(
-                    onClick = { onNavigateToReviewWrite() }
-                ) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.ic_review_write),
-                        contentDescription = stringResource(id = R.string.img_review_write_description),
-                        tint = Color.Gray
-                    )
-                }
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_review_write),
+                    contentDescription = stringResource(id = R.string.img_review_write_description),
+                    tint = Color.Gray,
+                    modifier = Modifier.clickable { onNavigateToReviewWrite() }
+                )
                 // Image -> 클릭 시 삭제하기 팝업창 Image 위에나 아래에 떠야함.
                 IconButton(
                     onClick = { showDialog = true }
@@ -250,6 +260,7 @@ fun ReviewItem(
                 // 팝업이 표시될 때
                 if (showDialog) {
                     ReviewItemDeletePopup(
+                        anchorPosition = buttonOffset,
                         onDeleteConfirm = {
                             // 삭제 확인 로직을 여기에 추가
                             showDialog = false // 팝업 닫기
@@ -266,14 +277,26 @@ fun ReviewItem(
 
 @Composable
 fun ReviewItemDeletePopup(
+    anchorPosition: Offset, // Position of the button
     onDeleteConfirm: () -> Unit, // 삭제 확인 시 호출되는 콜백 함수
     onDismiss: () -> Unit // 팝업 닫기 시 호출되는 콜백 함수
 ) {
-    Dialog(onDismissRequest = { onDismiss() }) {
+    Timber.i("anchorPosition: $anchorPosition, x: ${anchorPosition.x}, y: ${anchorPosition.y}")
+
+    Popup(
+        alignment = Alignment.TopStart,
+        offset = IntOffset(
+            x = -32, // Align the popup with the button
+            y = 120 // Adjust as needed based on the item's position
+        ),
+        onDismissRequest = onDismiss
+    ) {
         Surface(
             shape = RoundedCornerShape(4.dp),
             color = Color.White,
-            modifier = Modifier.height(34.dp)
+            modifier = Modifier
+                .clip(RoundedCornerShape(4.dp))
+                .border(1.dp, Gallery, RoundedCornerShape(4.dp))
         ) {
             Column(
                 verticalArrangement = Arrangement.Center,
