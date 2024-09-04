2a67ceaa4163f8d20c5a0ce348b60cfea00958a2
kamamerona <zszs2020@naver.com>
2024-09-04 23:19:42 +0900

    [feat] #62 해쉬키 사용

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,6 +9,14 @@ plugins {
 }
 
 android {
+    signingConfigs {
+        getByName("debug") {
+            storeFile = file("/Users/kamamerona/APK KEY/desserttime.jks")
+            storePassword = "123456"
+            keyAlias = "key0"
+            keyPassword = "123456"
+        }
+    }
     namespace = "com.desserttime.desserttimeapp"
 
     buildFeatures {
