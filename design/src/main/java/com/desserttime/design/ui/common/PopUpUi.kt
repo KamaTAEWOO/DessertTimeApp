e1797639f50eedb028c6b1be2c0dc4f992ea72bf
KamaTAEWOO <zszs2020@naver.com>
2024-10-29 22:08:52 +0900

    [feat] 화면 에러 팝업 생성

--- design/src/main/java/com/desserttime/design/ui/common/PopUpUi.kt
+++ design/src/main/java/com/desserttime/design/ui/common/PopUpUi.kt
@@ -1,10 +1,14 @@
 package com.desserttime.design.ui.common
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -13,6 +17,7 @@ import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Divider
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -21,12 +26,17 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Popup
+import com.desserttime.design.R
 import com.desserttime.design.theme.AzureRadiance
 import com.desserttime.design.theme.DessertTimeTheme
 import com.desserttime.design.theme.MainColor
+import com.desserttime.design.theme.WildSand
 
 object PopUpUi {
 
@@ -206,4 +216,33 @@ object PopUpUi {
     }
 
     // Page Error
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    @Composable
+    fun PageErrorPopup() {
+        Scaffold(
+            modifier = Modifier
+                .background(WildSand)
+                .fillMaxSize()
+        ) {
+            // Center the Column in both vertical and horizontal directions
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier.fillMaxSize()
+            ) {
+                // Image
+                Image(
+                    imageVector = ImageVector.vectorResource(id = R.drawable.ic_page_error),
+                    contentDescription = stringResource(id = R.string.txt_error),
+                )
+                // Text
+                Text(
+                    text = stringResource(id = R.string.txt_error),
+                    style = DessertTimeTheme.typography.textStyleRegular18,
+                    color = Color.Gray,
+                    modifier = Modifier.padding(top = 12.dp, bottom = 250.dp)
+                )
+            }
+        }
+    }
 }
