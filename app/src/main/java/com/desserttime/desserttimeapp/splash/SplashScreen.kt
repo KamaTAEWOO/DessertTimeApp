3a69a5105baa0a7cd845c3c8793256b2a337701d
KamaTAEWOO <zszs2020@naver.com>
2024-12-08 00:22:29 +0900

    [refactor] #129 merge request template 수정

--- app/src/main/java/com/desserttime/desserttimeapp/splash/SplashScreen.kt
+++ app/src/main/java/com/desserttime/desserttimeapp/splash/SplashScreen.kt
@@ -4,14 +4,13 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.tooling.preview.Preview
 import com.desserttime.design.theme.MainColor
 import com.desserttime.desserttimeapp.R
 
@@ -28,12 +27,17 @@ fun SplashScreen(onTimeout: () -> Unit) {
             .background(MainColor)
     ) {
         Image(
-            painter = painterResource(id = R.drawable.ic_screen_logo),
+            painter = painterResource(id = R.drawable.ic_splash_icon),
             contentDescription = R.string.app_name.toString(),
             modifier = Modifier
                 .align(Alignment.Center)
-                .height(84.dp)
-                .width(233.dp)
+                .wrapContentSize()
         )
     }
+}
+
+@Preview
+@Composable
+fun SplashScreenPreview() {
+    SplashScreen {}
 }
